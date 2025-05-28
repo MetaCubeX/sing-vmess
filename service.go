c7502891612811b512b51c70c692d95d10dd90cd
世界 <i@sekai.icu>
2025-05-28 21:29:34 +0800

    Fix request buffer

--- service.go
+++ service.go
@@ -339,7 +339,7 @@ func (s *Service[U]) NewConnection(ctx context.Context, conn net.Conn, metadata 
 		return err
 	}
 	if !legacyProtocol && requestBuffer.Len() > 0 {
-		reader = bufio.NewCachedReader(reader, requestBuffer)
+		reader = bufio.NewCachedReader(reader, requestBuffer.ToOwned())
 	}
 	reader = CreateReader(reader, nil, requestBodyKey, requestBodyNonce, requestBodyKey, requestBodyNonce, security, option)
 	if option&RequestOptionChunkStream != 0 && command == CommandTCP || command == CommandMux {
